cc2dc9b139071e5c27a7500f9863f72abfed55e8
jobsonvalney <jobsonvalney@hotmail.com>
2022-05-08 11:19:02 -0300

    1 commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from tokenize import String
+
+
+nome = "Baldo" 
+idade = 31 
+gosto_de = "programação"
+
+print (f"Meu nome é {nome}.")
+print (f"Eu gosto de {gosto_de}")
+print (f"Eu tenho {idade} anos")
+print (f"{nome} gosta de {gosto_de} e tem {idade} anos.")
